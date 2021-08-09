d0f2863afcf356385c4e1925b0a2bb9a567ffb8d
danembb <dbrophy720@gmail.com>
2021-08-08 18:36:07 -0600

    Create testing for merchant bd create, configure routes and controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,4 +4,8 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discounts = @merchant.bulk_discounts
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
