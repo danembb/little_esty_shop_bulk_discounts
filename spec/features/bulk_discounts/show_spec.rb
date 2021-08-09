4eba2e138d93698a3a690928baa3a45a125ed773
danembb <dbrophy720@gmail.com>
2021-08-09 01:06:42 -0600

    Create edit view and update show view and test files

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -58,17 +58,17 @@ RSpec.describe 'merchant bulk discount show page' do
   # When I visit my bulk discount show page x
   # Then I see a link to edit the bulk discount x
   # When I click this link x
-  # Then I am taken to a new page with a form to edit the discount t
+  # Then I am taken to a new page with a form to edit the discount x
   # And I see that the discounts current attributes are pre-poluated in the form
   # When I change any/all of the information and click submit
   # Then I am redirected to the bulk discount's show page
   # And I see that the discount's attributes have been updated
   it 'can edit a bulk discount' do
     expect(page).to have_link("Edit This Discount")
-    save_and_open_page
+
     click_on("Edit This Discount")
 
-    expect(current_path).to eq(edit_merchant_bulk_discount_path)
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, @bulk_discount1))
 
 
   end
